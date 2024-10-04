c9c72c38a76cc80d6da793bc0d5c07754ae09640
GabrielKall <gabrielkallmeyer@gmail.com>
2024-10-04 15:17:10 -0300

    Update MainImagem.java

--- src/MainImagem.java
+++ src/MainImagem.java
@@ -7,36 +7,33 @@ import java.io.IOException;
 public class MainImagem {
     public static void main(String[] args) {
         try {
-            // Carregar a imagem original para o Flood Fill com Pilha (DFS)
             File arquivoImagemOriginal = new File("C:\\Users\\gabri\\IdeaProjects\\TDE1\\src\\casa.png");
             BufferedImage imagemPilha = ImageIO.read(arquivoImagemOriginal);
-
-            // Definir a cor nova (exemplo: vermelho para o método com Pilha)
+            
             Color corNovaPilha = new Color(255, 0, 0);
+            
+            int xInicial = 300;
+            int yInicial = 300;
 
-            // Escolher uma coordenada inicial que esteja dentro da casa.
-            int xInicial = 300; // Ajuste o valor para um ponto dentro da casa
-            int yInicial = 300; // Ajuste o valor para um ponto dentro da casa
-
-            // Aplicar Flood Fill na imagem usando Pilha (DFS)
+           
             FloodFillImagem.floodFill(imagemPilha, xInicial, yInicial, corNovaPilha);
 
-            // Salvar a imagem resultante do Flood Fill com Pilha
+            
             File arquivoImagemResultadoPilha = new File("C:\\Users\\gabri\\IdeaProjects\\TDE1\\src\\casaPintadaPilha.png");
             ImageIO.write(imagemPilha, "png", arquivoImagemResultadoPilha);
 
             System.out.println("Flood Fill (DFS) aplicado e imagem salva como 'imagem_pintada_pilha.png'");
 
-            // Carregar novamente a imagem original para o Flood Fill com Fila (BFS)
+            
             BufferedImage imagemFila = ImageIO.read(arquivoImagemOriginal);
 
-            // Definir a cor nova (exemplo: azul para o método com Fila)
+            
             Color corNovaFila = new Color(0, 0, 255);
 
-            // Aplicar Flood Fill na imagem usando Fila (BFS)
+           
             FloodFillImagemFila.floodFill(imagemFila, xInicial, yInicial, corNovaFila);
 
-            // Salvar a imagem resultante do Flood Fill com Fila
+           
             File arquivoImagemResultadoFila = new File("C:\\Users\\gabri\\IdeaProjects\\TDE1\\src\\imagem_pintada_fila.png");
             ImageIO.write(imagemFila, "png", arquivoImagemResultadoFila);
 
